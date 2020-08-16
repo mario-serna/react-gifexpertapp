c134d411366d66d0dd32570fc2d9e32e840ef8ad
Mario Serna <mario_sh95@hotmail.com>
2020-08-16 17:17:39 -0500

    add docs

---
+++ docs/precache-manifest.1696a31fdad268c04b4f5570530ec674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0f74893cbf2f455f347ad4546d6b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ed78f4de23ea645009",
+    "url": "/static/css/main.46b2d03b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ed78f4de23ea645009",
+    "url": "/static/js/main.e8b2e43b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
